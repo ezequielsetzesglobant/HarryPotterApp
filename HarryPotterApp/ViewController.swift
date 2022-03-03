e6a743a9e4179cb18ba9c6b7fdccdad0e6b331bb
ezequielsetzes <ezequiel.setzes@globant.com>
2022-03-03 17:23:44 -0300

    structure implementation

--- HarryPotterApp/ViewController.swift
+++ HarryPotterApp/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  HarryPotterApp
-//
-//  Created by Ezequiel Hernan Setzes on 02/03/2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
---
+++ Presentation/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import PresentationTests
+
+var tests = [XCTestCaseEntry]()
+tests += PresentationTests.allTests()
+XCTMain(tests)
