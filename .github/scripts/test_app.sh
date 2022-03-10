a38e5db539a37a03ca9a4e941a26311b2961be65
ezequielsetzes <ezequiel.setzes@globant.com>
2022-03-10 15:24:16 -0300

    test 6

--- .github/scripts/test_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -project HarryPotterApp.xcproject \
-            -scheme HarryPotterApp\
-            -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
-            clean test | xcpretty
