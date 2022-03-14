b5d032b48229946885dc7e00b75902b6de5ab439
Fernando Nicola <fernando.nicola@globant.com>
2022-03-14 16:15:22 -0300

    fixing test

--- .github/scripts/test_swift_package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-cd Domain; swift test --parallel; cd ..
-cd Data; swift test --parallel; cd ..
-cd Presentation; swift test --parallel; cd ..
--- Presentation/Package.swift
+++ Presentation/Package.swift
@@ -1,11 +1,10 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.5.0
 
 import PackageDescription
 
 let package = Package(
     name: "Presentation",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,12 +12,8 @@ let package = Package(
             targets: ["Presentation"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Presentation",
             dependencies: []),
